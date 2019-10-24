73ca6b7f36793a6b70af00f6a9fb6cc95d5d9d9d
Noman Sadiq <nomi.cs786@gmail.com>
2019-10-24 11:40:24 +0400

    update location and added web app servaice and appservice plan

--- main.tf
+++ main.tf
@@ -1,4 +1,37 @@
-resource "azurerm_resource_group" "acs-partner-stage" {
+resource "azurerm_resource_group" "applepay_rg_POC" {
   name     = "applepay-rg-POC"
-  location = "westeurope"
+  location = "${var.location}"
+}
+
+
+
+resource "azurerm_app_service_plan" "applepaytest_Webapp" {
+  name                = "applepaytest"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.applepay-rg-POC.name}"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+    family = "S"
+    capacity = 1
+    name = "S1"
+  }
+}
+
+resource "azurerm_app_service" "applepaytest_ASP" {
+  name                = "applepaytest-app-service"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.applepay_rg_POC.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.applepaytest_Webapp.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
 }
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "location" {
+    description = "Location for the resouces"
+    default = "North Central US"
+}
+
+
